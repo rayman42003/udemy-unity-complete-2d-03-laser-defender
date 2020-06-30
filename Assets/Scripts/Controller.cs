b21a55196473b8114f29c20c88db978fd740c451
Richmond Chang <changrichmond@gmail.com>
2020-06-29 23:16:35 -0700

    Exercise 06 - Add basic left right movement to the player

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Controller : MonoBehaviour
+{
+    private UnityEvent<Vector2> onControlInputted = new VectorEvent();
+
+    public void registerOnControlInputted(UnityAction<Vector2> action) {
+        onControlInputted.AddListener(action);
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        var xInput = Input.GetAxis("Horizontal");
+        var yInput = Input.GetAxis("Vertical");
+        onControlInputted.Invoke(new Vector2(xInput, yInput));
+    }
+}
---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+    [SerializeField]
+    private Controller controller;
+
+    private void Start() {
+        if (controller == null) {
+            controller = GetComponent<Controller>();
+        }
+        controller.registerOnControlInputted((dir) => Move(dir));
+    }
+
+    private void Move(Vector2 dir) {
+        var newXPos = transform.position.x + dir.x;
+        transform.position = new Vector2(newXPos, transform.position.y);
+    }
+}
