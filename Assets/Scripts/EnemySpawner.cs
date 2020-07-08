1366c891ffdb55d4f4774b9aaf95c82ff1b35b70
Richmond Chang <changrichmond@gmail.com>
2020-07-08 12:59:24 -0700

    Exercise 06 - Define a wave and set up an enemy spawner that spawns just the first wave currently

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<Wave> waves = null;
+
+    [SerializeField]
+    private int waveIndex = 0;
+
+    private List<bool> currWave;
+
+    private void Start() {
+        Wave wave = waves[waveIndex];
+        StartCoroutine(spawnWave(wave));
+    }
+
+    private IEnumerator spawnWave(Wave wave) {
+        for (int i = 0; i < wave.getNumEnemies(); i++) {
+            Path path = wave.getPath();
+            var enemy = Instantiate(wave.getEnemy(), path.getWaypoints()[0].position, Quaternion.identity);
+            Navigable navigable = enemy.GetComponent<Navigable>();
+            navigable.setPath(path);
+            navigable.setMoveSpeed(wave.getMoveSpeed());
+            yield return new WaitForSeconds(wave.getSpawnTime());
+        }
+    }
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave")]
+public class Wave : ScriptableObject
+{
+    [SerializeField]
+    private GameObject enemy = null;
+
+    [SerializeField]
+    private int numEnemies = 3;
+
+    [SerializeField]
+    private GameObject path = null;
+
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    [SerializeField]
+    private float spawnTime = 1f;
+
+    public GameObject getEnemy() {
+        return enemy;
+    }
+
+    public int getNumEnemies() {
+        return numEnemies;
+    }
+
+    public Path getPath() {
+        return path.GetComponent<Path>();
+    }
+
+    public float getMoveSpeed() {
+        return moveSpeed;
+    }
+
+    public float getSpawnTime() {
+        return spawnTime;
+    }
+}
