69d2509a343576654e80bdde42095b4c0a01f235
Richmond Chang <changrichmond@gmail.com>
2020-07-29 18:07:07 -0700

    Exercise 06 - Add scrolling to background:     -  Use quad and set background as texture to simulate scrolling using offset

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField]
+    private float backgroundSpeed = 0.1f;
+
+    private Material backgroundMaterial;
+
+    // Start is called before the first frame update
+    private void Start() {
+        backgroundMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        Vector2 offset = new Vector2(0f, backgroundSpeed * Time.deltaTime);
+        backgroundMaterial.mainTextureOffset += offset;
+    }
+}
