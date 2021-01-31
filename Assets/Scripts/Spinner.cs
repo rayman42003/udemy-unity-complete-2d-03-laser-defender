f32792a3f9ea711ec5cfa387085476b9e47356c8
Richmond Chang <changrichmond@gmail.com>
2021-01-30 18:29:32 -0800

    Exercise 06 - Make enemy bomb projectile spin

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 360f;
+
+    private void Update() {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+    }
+}
